db23a86c1fc70df5b6e5846795bf5bfc4a0b9f62
Ebrahim Bangdiwala <ebb.bang@gmail.com>
2023-11-30 08:59:46 +0530

    setup routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { DevSupport } from "@react-buddy/ide-toolbox";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import App from "./App.tsx";
-import theme from "./theme.ts";
-import "./index.css";
-import { DevSupport } from "@react-buddy/ide-toolbox";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 import { ComponentPreviews, useInitial } from "./dev";
+import "./index.css";
+import router from "./routes.tsx";
+import theme from "./theme.ts";
 
 const queryClient = new QueryClient();
 
@@ -20,7 +21,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         useInitialHook={useInitial}
       >
         <QueryClientProvider client={queryClient}>
-          <App />
+          <RouterProvider router={router} />
           <ReactQueryDevtools />
         </QueryClientProvider>
       </DevSupport>
